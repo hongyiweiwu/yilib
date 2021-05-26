2cbd2481fcebbedf9860461325c63bfe310e3cb9
Yi Hong <hongyiweiwu@outlook.com>
2021-05-26 12:26:00 -0400

    Implemented "stdexcept.hpp".

--- include/string.hpp
+++ include/string.hpp
@@ -1,6 +1,14 @@
 #pragma once
 
 namespace std {
-    // TODO: This is a temporary alias so other headers using string can compile.
-    using string = const char*;
+    template<class CharT> class basic_string {
+    public:
+        constexpr basic_string(const CharT*) {}
+
+        constexpr const CharT* c_str() const noexcept {
+            return "Unimplemeneted";
+        }
+    };
+
+    using string = basic_string<char>;
 }
---
+++ src/stdexcept.cpp
@@ -0,0 +1,36 @@
+#include "stdexcept.hpp"
+#include "string.hpp"
+#include "exception.hpp"
+
+namespace std {
+    logic_error::logic_error(const string& what_arg) : exception(), what_str(what_arg.c_str()) {}
+    logic_error::logic_error(const char* what_arg) : exception(), what_str(what_arg) {}
+
+    const char* logic_error::what() const noexcept { return what_str; }
+
+    domain_error::domain_error(const string& what_arg) : logic_error(what_arg) {}
+    domain_error::domain_error(const char* what_arg) : logic_error(what_arg) {}
+
+    invalid_argument::invalid_argument(const string& what_arg) : logic_error(what_arg) {}
+    invalid_argument::invalid_argument(const char* what_arg) : logic_error(what_arg) {}
+
+    length_error::length_error(const string& what_arg) : logic_error(what_arg) {}
+    length_error::length_error(const char* what_arg) : logic_error(what_arg) {}
+
+    out_of_range::out_of_range(const string& what_arg) : logic_error(what_arg) {}
+    out_of_range::out_of_range(const char* what_arg) : logic_error(what_arg) {}
+
+    runtime_error::runtime_error(const string& what_arg) : exception(), what_str(what_arg.c_str()) {}
+    runtime_error::runtime_error(const char* what_arg) : exception(), what_str(what_arg) {}
+
+    const char* runtime_error::what() const noexcept { return what_str; }
+
+    range_error::range_error(const string& what_arg) : runtime_error(what_arg) {}
+    range_error::range_error(const char* what_arg) : runtime_error(what_arg) {}
+
+    overflow_error::overflow_error(const string& what_arg) : runtime_error(what_arg) {}
+    overflow_error::overflow_error(const char* what_arg) : runtime_error(what_arg) {}
+
+    underflow_error::underflow_error(const string& what_arg) : runtime_error(what_arg) {}
+    underflow_error::underflow_error(const char* what_arg) : runtime_error(what_arg) {}
+}
