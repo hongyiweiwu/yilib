0974ab09fa8293e4181a2f6dd31c7ce065d296e3
Yi Hong <hongyiweiwu@outlook.com>
2021-09-24 23:49:39 -0400

    Replaced implementation of addressof to make it constexpr.

--- include/memory/pointer_util.hpp
+++ include/memory/pointer_util.hpp
@@ -7,19 +7,7 @@ namespace std {
     /* 20.10.11 addressof */
     template<class T>
     constexpr T* addressof(T& r) noexcept {
-        if constexpr (is_object_v<T>) {
-            // And we cast back to the desired T* type.
-            return reinterpret_cast<T*>(
-                // And then we remove the cv-qualifiers, and take the address -- we know char doesn't have an overload & operator.
-                &const_cast<char&>(
-                    // First we cast r to the most permissible (cv-qualified) char type.
-                    reinterpret_cast<const volatile char&>(r)
-                )
-            );
-        } else {
-            // For non-object types, overloading & is impossible so we can safely use it.
-            return &r;
-        }
+        return __builin_addressof(r);
     }
 
     template<class T>
